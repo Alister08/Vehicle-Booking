7e1cf03327417dbb93f156c7252edf64f03f6386
Alister08 <alisteralexhosamani@gmail.com>
2025-05-17 22:38:12 +0530

    formType added minor fix

--- client/vehicle-booking/src/actions/vehiclebooking.ts
+++ client/vehicle-booking/src/actions/vehiclebooking.ts
@@ -2,13 +2,17 @@
 import axios from 'axios'
 import { toast } from 'react-hot-toast'
 import { api } from "./api";
+import { vehicleBookingSchema } from '@/lib/zod/vehiclebooking';
+import { z } from 'zod';
 
 // import { FormValues } from '@/components/VehicleBookingForm'
+type FormValues = z.infer<typeof vehicleBookingSchema>;
+
 
 /**
  * Sends the booking payload to your API and shows a toast.
  */
-export async function createBooking(data:any) {
+export async function createBooking(data:FormValues) {
   try {
     const res = await api.post('/booking/create-booking', data)
     toast.success('🎉 Booking confirmed!')
